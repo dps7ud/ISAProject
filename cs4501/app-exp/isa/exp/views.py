75ca33eebb515e4f518397354bda6fb5fded327b
bjw4ph <bjw4ph@virginia.edu>
2017-02-20 01:46:34 -0500

    set up home endpoint in experience service to get topUsers and recentListings

--- cs4501/app-exp/isa/exp/views.py
+++ cs4501/app-exp/isa/exp/views.py
@@ -1,3 +1,24 @@
+from django.core import serializers
 from django.shortcuts import render
+from django.http import HttpResponse
+
+import urllib.request
+import urllib.parse
+import json
 
 # Create your views here.
+def home(request):
+	req = urllib.request.Request('http://models-api:8000/topUsers')
+	resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+	resp = json.loads(resp_json)
+	topUsers = []
+	for i in resp:
+		topUsers.append(i)
+	req2 = urllib.request.Request('http://models-api:8000/recentListings')
+	resp_json2 = urllib.request.urlopen(req2).read().decode('utf-8')
+	resp2 = json.loads(resp_json2)
+	recentListings = []
+	for j in resp2:
+		recentListings.append(j)
+	data = json.dumps([topUsers, recentListings])
+	return HttpResponse(data)
