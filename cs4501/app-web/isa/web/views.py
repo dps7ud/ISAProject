842d8808212e5b9dd04a0cb30df14b3c45a13dd3
bjw4ph <bjw4ph@virginia.edu>
2017-02-20 02:32:38 -0500

    set up home page template to display top 5 users and 5 most recent listings

--- cs4501/app-web/isa/web/views.py
+++ cs4501/app-web/isa/web/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.core import serializers
+from django.http import HttpResponse
+
+import urllib.request
+import urllib.parse
+import json
+
 
 # Create your views here.
+def home(request):
+	req = urllib.request.Request('http://exp-api:8000/home/')
+	resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+	resp = json.loads(resp_json)
+	topUsers = []
+	for i in resp[0]:
+		topUsers.append(i)
+	latestListings = []
+	for j in resp[1]:
+		latestListings.append(j)
+	context = {
+		'top_users_list': topUsers,
+		'recent_listings_list': latestListings
+	}
+	# return HttpResponse(topUsers)
+	return render(request, 'web/home.html', context)
+
