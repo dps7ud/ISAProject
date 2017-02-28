b5a99ff44264a17cd05afa1e829c8b9d7d52a3e5
dps7ud <dps7ud@virginia.edu>
2017-02-28 01:45:26 -0500

    Optional slash so I don't go insane

--- web_app/web_app/urls.py
+++ web_app/web_app/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^user/(?P<user_id>[0-9]+)/$', views.user, name='user'),
-	url(r'^task/(?P<task_id>[0-9]+)/$', views.task, name='task'),
-    url(r'^review/(?P<review_id>[0-9]+)/$', views.review, name='review'),
+	url(r'^user/(?P<user_id>[0-9]+)/?$', views.user, name='user'),
+	url(r'^task/(?P<task_id>[0-9]+)/?$', views.task, name='task'),
+    url(r'^review/(?P<review_id>[0-9]+)/?$', views.review, name='review'),
     url(r'^', views.home, name='home'),
     
 ]
