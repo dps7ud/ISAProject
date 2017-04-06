45e25409802e51c7fe3470e7c256a6d188830ffd
bjw4ph <bjw4ph@virginia.edu>
2017-04-06 14:39:59 -0400

    extend base.html to all other templates

--- web_app/static/js/setup.js
+++ web_app/static/js/setup.js
@@ -7,30 +7,29 @@ $(document).ready(function(){
 //         $("#nav1").parent().addClass("active");
 //     }
 // });
-$("#navbar").load("/static/html/navbar.html", function(){
-    var pathname = window.location.pathname;
-    console.log($("#authPresent").data("auth"))
-    if($("#authPresent").data("auth") == "yes"){
-        console.log("if statement")
-        $("#nav5").html("Logout")
-        $("#nav5").attr("href", "/logout")
-    } 
-    if (pathname.indexOf("/task") >= 0){
-        $("#nav1").parent().addClass("active");
-    }
-    if (pathname.indexOf("/user") >= 0){
-        $("#nav2").parent().addClass("active");
-    }
-    if (pathname.indexOf("/review") >= 0){
-        $("#nav3").parent().addClass("active");
-    }
-    if (pathname.indexOf("/login") >= 0){
-        $("#nav5").parent().addClass("active");
-    }
-    if (pathname.indexOf("/signup") >= 0){
-        $("#nav4").parent().addClass("active");
-    }
-});
+var pathname = window.location.pathname;
+console.log($("#authPresent").data("auth"))
+if($("#authPresent").data("auth") == "yes"){
+    console.log("if statement")
+    $("#nav5").html("Logout")
+    $("#nav5").attr("href", "/logout")
+} 
+if (pathname.indexOf("/task") >= 0){
+    $("#nav1").parent().addClass("active");
+}
+if (pathname.indexOf("/user") >= 0){
+    $("#nav2").parent().addClass("active");
+}
+if (pathname.indexOf("/review") >= 0){
+    $("#nav3").parent().addClass("active");
+}
+if (pathname.indexOf("/login") >= 0){
+    $("#nav5").parent().addClass("active");
+}
+if (pathname.indexOf("/signup") >= 0){
+    $("#nav4").parent().addClass("active");
+}
+
 
 // console.log(pathname)
 
