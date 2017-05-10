8af73bbca45ac28381715779c1ebd276faf16103
bjw4ph <bjw4ph@virginia.edu>
2017-05-10 01:34:13 -0400

    windows automation script start, removing duplicates in the spark job

---
+++ spark_start_windows.sh
@@ -0,0 +1,9 @@
+# Install mysqldb and python3 on masters and clients
+# Found locally at .../ISAProject/batch/spark_setup.sh
+docker exec -it spark-master bash -c "apt-get update && apt-get install python3-dev libmysqlclient-dev -y && apt-get install python-pip -y && pip install mysqlclient && apt-get install python-mysqldb"
+docker exec -it spark-worker bash -c "apt-get update && apt-get install python3-dev libmysqlclient-dev -y && apt-get install python-pip -y && pip install mysqlclient && apt-get install python-mysqldb"
+# Start job
+docker exec -it spark-master bin/spark-submit --master spark://spark-master:7077 --total-executor-cores 2 --executor-memory 512m /tmp/data/hello.py
+
+# Perform job every 120 seconds
+# watch -n 120 docker exec -it spark-master bin/spark-submit --master spark://spark-master:7077 --total-executor-cores 2 --executor-memory 512m /tmp/data/hello.py
