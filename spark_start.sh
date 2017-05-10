eb5d22c8df198e6c78262c3cc1e39ac1807b679c
dps7ud <dps7ud@virginia.edu>
2017-05-10 10:09:05 -0400

    platform independent spark_start

--- spark_start.sh
+++ spark_start.sh
@@ -1,9 +1,15 @@
 # Install mysqldb and python3 on masters and clients
 # Found locally at .../ISAProject/batch/spark_setup.sh
-docker exec -it spark-master /tmp/data/spark_setup.sh
-docker exec -it spark-worker /tmp/data/spark_setup.sh
-# Start job
+#docker exec -it spark-master /tmp/data/spark_setup.sh
+#docker exec -it spark-worker /tmp/data/spark_setup.sh
+docker exec -it spark-master bash /tmp/data/spark_setup.sh
+docker exec -it spark-worker bash /tmp/data/spark_setup.sh
+## Start job
 docker exec -it spark-master bin/spark-submit --master spark://spark-master:7077 --total-executor-cores 2 --executor-memory 512m /tmp/data/hello.py
 
 # Perform job every 120 seconds
-watch -n 120 docker exec -it spark-master bin/spark-submit --master spark://spark-master:7077 --total-executor-cores 2 --executor-memory 512m /tmp/data/hello.py
+#watch -n 120 docker exec -it spark-master bin/spark-submit --master spark://spark-master:7077 --total-executor-cores 2 --executor-memory 512m /tmp/data/hello.py
+while true; do
+    sleep 120
+    docker exec -it spark-master bin/spark-submit --master spark://spark-master:7077 --total-executor-cores 2 --executor-memory 512m /tmp/data/hello.py
+done
